f193e20f3c5737187b15ec55e7001facbe9fbade
Cedric Breuning <mail@ccbreuning.de>
2023-10-28 18:59:39 +0200

    update example

--- examples/example.py
+++ examples/example.py
@@ -1,31 +1,37 @@
-# Nur um Pfade richtig verwenden zu können, kann man auch einfach mit Strings machen
 from pathlib import Path
 
 import numpy as np
+import uncertainties.unumpy as unp
 
 from pround.pround import Pround
 
-# Ein Paar Pseudo Daten
+# Random data
 x = np.array([1.23456789, 2.3456789, 3.456789])
 dx = np.array([0.123456789, 0.23456789, 0.3656789])
 y = np.array([10.23456789, 20.3456789, 30.456789])
 dy = np.array([1.23456789, 2.3456789, 3.456789])
 z = np.array([42, 21, 100])
 
-# Initialisiere das Objekt um die Tabelle zu erstellen
-p = Pround(format="latex") # als format geht auch excel, wenn es in eine excel Tabelle geschrieben werden soll
+# Create the object das Objekt um die Tabelle zu erstellen
+# If you later want an excel table use "excel" instead of "latex"
+p = Pround(format="latex") 
 
-# Alle daten werden als neue Spalte hinzugefügt. Der String am Anfang steht nachher als Überschrift der Spalte in der Tabelle.
-# Gerundet wird an der ersten signifikanten Stelle des Fehlers (sollte das eine 1 oder eine 2 sein, dann wird eine stelle weiter gerundet). Da das uncertainties package verwendet wird, wird auch beim Fehler normal (und nicht immer auf) gerundet
-p.add_column("x / cm", x, dx)
-p.add_column("y / cm", y, dy)
+# All data are added in columns. The first argument will be the columns name in the table.
+# THus, all arrays must have the same length
+# The rounding is specified in the README
+p.add_column("x / cm", x, uncertainties=dx)
 
-# Man kann auch Werte ohne Fehler angeben. ndigits gibt dann an wo gerundet werden soll
+
+# You can also provide values without uncertainties and the values are rounded to the specified number of digits (default is 2) after the decimal point
 p.add_column("z / cm", z, ndigits=1)
 
-# Pfad zum Verzeichnis dieses Python skripts
+# You can also add uarrays from uncertainties
+y_unc = unp.uarray(y, dy)
+p.add_column("y / cm", y_unc)
+
+# Path to this file
 cwd = Path(__file__).parent
 
-# Speicer das ganze als tex datei ab. landscape gibt an ob das Dokument im Querformat sein soll
-# Man kann filename auch weglassen, dann wird die Tabelle (ohne Präambel und so) im Terminal gedruckt
+# Save table in latex file (can be complied with e.g. lualatex). Landscape is optional and defines the orientation of the latex file
+# If filename is not provided the table is printed to stdout
 p.print_table(filename=cwd / "test.tex", landscape=True)
